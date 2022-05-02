cb0de636ec0ff0f2f02a102fcddb8bf757cb8b57
aisaack <jazzrhythm@naver.com>
2022-05-02 23:08:33 +0900

    adding data augmentation

---
+++ augmentation.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from sklearn.decomposition import PCA
+
+def random_crop(img, *args):
+    size = tf.shape(img)
+    img = tf.pad(img, [[4, 4], [4, 4], [0, 0]])
+    img = tf.image.random_crop(img, size)
+    return [img] + [arg for arg in args]
+
+def noising(img, *args):
+    pca = PCA(32, svd_solver='full')
+    noise = np.random.normal(0, 0.1, [32, 32])
+    noise = pca.fit(noise).components_
+    noise = tf.stack([noise]*3, axis=-1)
+    noise = tf.cast(noise, tf.float32)
+    return [img + noise] + [arg for arg in args]
+
+def h_flip(img, *args):
+    img = tf.image.random_flip_left_right(img)
+    return [img] + [arg for arg in args]
--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,19 @@
 import tensorflow as tf
+from augmentation import (random_crop, noising, h_flip)
 
 def dataloader(
         batch_size = None,
         validation_split = 0.1,
-        preprocessing = None,
+        augmentation = None,
         mode = 'train'
     ):
+    for idx, aug in enumerate(augmentation):
+        assert aug in ['random_crop', 'pca_noising', 'h_flip']
+        if aug == 'h_flip' and idx != -1:
+            augmentation[idx], augmentation[-1] = augmentation[-1], aug, 
+    
+    print(augmentation)
+
     
     if batch_size is None:
         batch_size = int(len(feature))
@@ -25,11 +33,24 @@ def dataloader(
             train_ds = ds.skip(val_len)
             ds = train_ds
 
-        if preprocessing:
-            ds = ds.apply(preprocessing).shuffle(100)
+        concat = [ds]
+        for idx, aug in enumerate(augmentation):
+            if aug == 'random_crop':
+                x = ds.map(random_crop)
+            elif aug == 'pca_noising':
+                x = ds.map(noising)
+            elif aug == 'h_flip':
+                for idx, data in enumerate(concat[:3]):
+                    print(idx)
+                    x = data.shard(num_shards=2, index=tf.cast(tf.where(tf.random.uniform([], 0, 1)>0.5, 1, 0), 'int64')).map(h_flip)
+                    concat.append(x)
+            if idx < len(augmentation)-1:
+                concat.append(x)
+        for data in concat[1:]:
+            ds = ds.concatenate(data)
 
-        ds = ds.batch(batch_size = batch_size, drop_remainder = True).prefetch(1)
-
+        ds = ds.cache()
+        ds = ds.shuffle(100).batch(batch_size = batch_size, drop_remainder = True).prefetch(1)
         if validation_split:
             return ds, val_ds
         return ds
@@ -48,3 +69,13 @@ def dataloader(
         return test
     elif mode == 'inference':
         return inference
+
+if __name__ == '__main__':
+    from tensorflow.keras.datasets.cifar10 import load_data
+    (x_train, y_train), (x_test, y_test) = load_data()
+    loader = dataloader(batch_size = 256,
+                        validation_split = 0.3,
+                        augmentation=['random_crop', 'h_flip', 'pca_noising'],
+                        mode = 'train')
+    train_ds, val_ds = loader(x_train, y_train)
+    print(train_ds.cardinality(), val_ds.cardinality())
